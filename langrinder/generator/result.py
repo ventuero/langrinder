d1bf8982b0011b8daee5a6ac0cb811eab7908a36
tirch <247108189-tirch@users.noreply.gitlab.com>
2025-06-01 12:33:20 +0300

    added flex gender config

--- langrinder/generator/result.py
+++ langrinder/generator/result.py
@@ -3,10 +3,12 @@ import time
 from dataclasses import dataclass
 
 from mako.template import Template
+from telegrinder.tools.global_context import GlobalContext, GlobalCtxVar
 
 from langrinder.tools import GenderGenerator
 
 logger = logging.getLogger(__name__)
+ctx = GlobalContext("langrinder")
 
 
 @dataclass
@@ -17,8 +19,16 @@ class TextResult:
     def __call__(self, **kwargs):
         start = time.perf_counter()
         if kwargs.get("gender"):
-            gender_gen = GenderGenerator(kwargs["gender"])
-            kwargs["gender"] = gender_gen
+            gender_gen_type = (
+                ctx
+                .get("gender_generator")
+                .unwrap_or_none()
+            )
+            if gender_gen_type:
+                gender_gen = gender_gen_type.value(kwargs["gender"])
+            else:
+                gender_gen = GenderGenerator(kwargs["gender"])
+            kwargs["gender"] = gender_gen.gender
         rendered = self.template.render(
             **self.args,
             **kwargs,
